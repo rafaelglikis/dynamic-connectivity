f37b4dcd7f516b40d9e5d41ebb7fdce20e55fc7d
rafaelglikis <rafaelglikis@gmail.com>
2018-07-11 11:06:40 +0300

    minor changes

---
+++ benchmarks.sh
@@ -0,0 +1,13 @@
+./bin/dc benchmark --path -v 1000 -d 500 -q 500
+./bin/dc benchmark --path -v 10000 -d 5000 -q 5000
+./bin/dc benchmark --path -v 100000 -d 50000 -q 50000
+./bin/dc benchmark --tree -v 1000 -d 500 -q 500
+./bin/dc benchmark --tree -v 10000 -d 5000 -q 5000
+./bin/dc benchmark --tree -v 100000 -d 50000 -q 50000
+./bin/dc benchmark --random -v 1000  -d 500 -q 500
+./bin/dc benchmark --random -v 10000 -d 5000 -q 5000
+./bin/dc benchmark --random -v 100000 -d 50000 -q 50000
+./bin/dc benchmark --path-complete -v 1000  -d 500 -q 500
+./bin/dc benchmark --path-complete -v 5000 -d 5000 -q 5000
+./bin/dc benchmark --path-complete -v 10000 -d 50000 -q 50000
+
