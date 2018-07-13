cc7cc96f0c49578b6782ed92d4658ef6799d13c1
rafaelglikis <rafaelglikis@gmail.com>
2018-07-13 17:19:09 +0300

    added boost parser

--- src/main.cpp
+++ src/main.cpp
@@ -9,107 +9,80 @@
 #include "../incl/benchmarks/TreeBenchmark.h"
 #include "../incl/benchmarks/PathCompleteGraphsBenchmark.h"
 #include <cmath>
+#include <boost/program_options.hpp>
+
+unsigned long vertices;
+unsigned long edges;
+unsigned long deletions;
+unsigned long queries;
+
+boost::program_options::variables_map parseArguments(int argc, char *const *argv)
+{
+    boost::program_options::options_description options("Usage");
+    options.add_options()
+            ("test", "run all tests")
+            ("benchmark", "run benchmark specified benchmarks")
+            ("path", "run benchmark with path graph")
+            ("tree", "run benchmark with tree graph")
+            ("random", "run benchmark with random graph")
+            ("path-complete", "run benchmark with path of complete subgraphs")
+            ("vertices,v", boost::program_options::value<unsigned long>(&vertices)
+                    ->default_value(100), "specify number of vertices (default 100)")
+            ("edges,e", boost::program_options::value<unsigned long>(&edges)
+                     ->default_value(static_cast<const unsigned long &>(100*log(100))),
+             "specify number of edges (default vertices*log(vertices)) (only for random graphs)")
+            ("deletions,d", boost::program_options::value<unsigned long>(&deletions)
+                    ->default_value(50), "specify number of vertices (default 50)")
+            ("queries,q", boost::program_options::value<unsigned long>(&queries)
+                    ->default_value(50), "specify number of vertices (default 50)")
+            ("example", "run an example")
+            ("help", "produce help message")
+            ;
+
+    boost::program_options::variables_map vm;
+    store(parse_command_line(argc, argv, options), vm);
+    notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << options << "\n";
+        exit(1);
+    }
+
+    return vm;
+}
 
 int main(int argc, char* argv[])
 {
-    // Parse arguments
-    bool test = false;
-    bool example = false;
-    bool benchmark = false;
-    bool path = false;
-    bool tree = false;
-    bool random = false;
-    bool partComplete = false;
-    bool help = false;
+    boost::program_options::variables_map args = parseArguments(argc, argv);
 
-    unsigned long vertices = 10000;
-    unsigned long edges = static_cast<unsigned long>(vertices*log(vertices));
-    unsigned long deletes = 5000;
-    unsigned long queries = 5000;
-
-    for(int i=0; i<argc; ++i) {
-        if(std::string(argv[i])=="-v" || std::string(argv[i])=="--vertices")
-            vertices= static_cast<unsigned long>(atol(argv[i+1]));
-        else if(std::string(argv[i])=="-e" || std::string(argv[i])=="--edges")
-            edges= static_cast<unsigned long>(atol(argv[i+1]));
-        else if(std::string(argv[i])=="-d" || std::string(argv[i])=="--deletions")
-            deletes= static_cast<unsigned long>(atol(argv[i+1]));
-        else if(std::string(argv[i])=="-q" || std::string(argv[i])=="--queries")
-            queries= static_cast<unsigned long>(atol(argv[i+1]));
-        else if(std::string(argv[i])=="benchmark")
-            benchmark=true;
-        else if(std::string(argv[i])=="test")
-            test=true;
-        else if(std::string(argv[i])=="example")
-            example=true;
-        else if(std::string(argv[i])=="--path")
-            path=true;
-        else if(std::string(argv[i])=="--tree")
-            tree=true;
-        else if(std::string(argv[i])=="--random")
-            random=true;
-        else if(std::string(argv[i])=="--path-complete")
-            partComplete=true;
-        else if(std::string(argv[i])=="--help" || std::string(argv[i])=="-h")
-            help=true;
-    }
-
-    if (test) {
+    if (args.count("test")) {
         ActionTest actionTest;
         DynamicGraphTest graphTest;
         actionTest.run();
         graphTest.run();
     }
-    if (benchmark) {
-        if (path) {
+    if (args.count("benchmark")) {
+        if (args.count("path")) {
             PathBenchmark pathBenchmark(vertices);
-            pathBenchmark.run(deletes, queries);
+            pathBenchmark.run(deletions, queries);
         }
-        if (tree) {
+        if (args.count("tree")) {
             TreeBenchmark treeBenchmark(vertices);
-            treeBenchmark.run(deletes, queries);
+            treeBenchmark.run(deletions, queries);
         }
-        if (random) {
+        if (args.count("random")) {
             RandomGraphBenchmark randomGraphBenchmark(vertices, edges);
-            randomGraphBenchmark.run(deletes, queries);
+            randomGraphBenchmark.run(deletions, queries);
         }
-        if (partComplete) {
+        if (args.count("path-complete")) {
             PathCompleteGraphsBenchmark pathCompleteGraphsBenchmark(
                     static_cast<int>(sqrt(vertices)),
                     static_cast<int>(sqrt(vertices)));
-            pathCompleteGraphsBenchmark.run(deletes, queries);
+            pathCompleteGraphsBenchmark.run(deletions, queries);
         }
     }
-    if (example) {
+    if (args.count("example")) {
         runExample();
-    }
-
-    if (help || argc < 2) {
-        std::cout << "Usage: " << std::endl;
-        std::cout << "  " << argv[0] << " ACTION* [OPTION]*" << std::endl;
-        std::cout << std::endl;
-        std::cout << "Example: " << std::endl;
-        std::cout << "  " << argv[0] << " benchmark --random -v 500 -e 1000 -d 50 -q 50" << std::endl;
-        std::cout << std::endl;
-        std::cout << "Test: " << std::endl;
-        std::cout << std::left << std::setw(30) <<"  test " << "run all tests" << std::endl;
-        std::cout << std::endl;
-        std::cout << "Benchmark: " << std::endl;
-        std::cout << std::left << std::setw(30) <<"  benchmark" << "run benchmark specified benchmarks" << std::endl;
-        std::cout << std::left << std::setw(30) <<"  --path" << "run benchmark with path graph" << std::endl;
-        std::cout << std::left << std::setw(30) <<"  --tree" << "run benchmark with tree graph" << std::endl;
-        std::cout << std::left << std::setw(30) <<"  --random" << "run benchmark with tree graph" << std::endl;
-        std::cout << std::left << std::setw(30) <<"  --path-complete" << "run benchmark with path of partly complete graphs" << std::endl;
-        std::cout << std::endl;
-        std::cout << "Benchmark options: " << std::endl;
-        std::cout << std::left << std::setw(30) <<"  -v, --vertices <number>" << "specify number of vertices (default 10000)" << std::endl;
-        std::cout << std::left << std::setw(30) <<"  -e, --edges <number>" << "specify number of edges (default 10000*log(10000))" << std::endl;
-        std::cout << std::left << std::setw(30) <<"  -d, --deletions <number>" << "specify number of deletions (default 5000)" << std::endl;
-        std::cout << std::left << std::setw(30) <<"  -q, --queries <number>" << "specify number of queries (default 5000)" << std::endl;
-        std::cout << std::endl;
-        std::cout << "Examples: " << std::endl;
-        std::cout << std::left << std::setw(30) <<"  example" << "run an example" << std::endl;
-        std::cout << std::endl;
     }
 
     return 0;
